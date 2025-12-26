f616b050f443ea48f6c333da5d22e99c0d3ac4b4
AlexAxenti <alex.axenti@gmail.com>
2025-12-26 17:20:49 -0500

    initial

---
+++ target/debug/deps/quick_cmd-88703701b6c74bf9.d
@@ -0,0 +1,5 @@
+C:\Users\CodeMaster\Desktop\quick_cmd\target\debug\deps\quick_cmd-88703701b6c74bf9.d: src\main.rs
+
+C:\Users\CodeMaster\Desktop\quick_cmd\target\debug\deps\libquick_cmd-88703701b6c74bf9.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/quick_cmd-f1ae03b40d2dd00e.d
@@ -0,0 +1,5 @@
+C:\Users\CodeMaster\Desktop\quick_cmd\target\debug\deps\quick_cmd-f1ae03b40d2dd00e.d: src\main.rs
+
+C:\Users\CodeMaster\Desktop\quick_cmd\target\debug\deps\libquick_cmd-f1ae03b40d2dd00e.rmeta: src\main.rs
+
+src\main.rs:
